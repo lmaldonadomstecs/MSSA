240fe227c549e3f6a1095fd46b982b90a420af09
L.Maldonado <lmaldonado@mstecs.com>
2021-10-29 15:22:07 -0600

    Assignment 1 for Programming in C# added.

---
+++ Assignments/3/Solution/js/script.js
@@ -0,0 +1,17 @@
+var url = "https://pokeapi.co/api/v2/pokemon/";
+var whichPokemon;
+
+function searchPokemon() {
+    whichPokemon = document.getElementById("searchbar").value;
+    whichPokemon = whichPokemon.toLowerCase();
+    url = "https://pokeapi.co/api/v2/pokemon/"
+    url += whichPokemon; // url = url + pokemon
+    fetch(url)
+    .then(response => response.json())
+    .then(pokemon => {
+        var parent = document.getElementById('parent');
+        parent.innerHTML = '<p>' + pokemon.name +'</p>';
+        parent.innerHTML += "<img src=" + pokemon.sprites.back_shiny + ">";
+        // console.log(pokemon);
+    }).catch(err => console.log(err))
+}
