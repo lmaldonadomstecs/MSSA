6438305ee1a669e777843d8ee8f91f2b31276d74
L.Maldonado <lmaldonado@mstecs.com>
2021-11-09 07:28:17 -0700

    Challenges for module 3 added.

---
+++ Activities/20483C-ProgramminginC#/Challenges/4Collections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hint
+            List<string> guests = new List<string>();
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            Queue<int> bankQueue = new Queue<int>();
+            Stack<int> calculator = new Stack<int>();
+        }
+    }
+}
