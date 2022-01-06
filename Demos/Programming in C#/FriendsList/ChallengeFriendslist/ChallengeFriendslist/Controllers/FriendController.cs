8a8980bea2ebc4d0c2f9893452c8c03c5fa3e2e5
L.Maldonado <lmaldonado@mstecs.com>
2022-01-06 12:37:51 -0700

    Friendslist added to Demos Programming in C#

---
+++ Demos/Programming in C#/FriendsList/ChallengeFriendslist/ChallengeFriendslist/Controllers/FriendController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChallengeFriendslist.Models;
+using ChallengeFriendslist.Services;
+
+namespace ChallengeFriendslist.Controllers
+{
+    public class FriendController : Controller
+    {
+        // We need the butler
+        IListOfFriends _listOfFriends;
+
+        // Inject service into Controller
+        public FriendController(IListOfFriends myListService)
+        {
+            // Dependency injection Reference type values
+            // FAUCET    connected to      PIPE
+            _listOfFriends = myListService;
+        }
+
+        // READ
+        public IActionResult Index()
+        {
+            return View(_listOfFriends.getFriends());
+        }
+
+        [HttpGet]
+        public IActionResult InsertNewFriend()
+        {
+            return View();
+        }
+
+        // CREATE FRIENDS
+        [HttpPost]
+        public IActionResult InsertNewFriend(Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                _listOfFriends.insertNewFriend(friend);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult FriendsDetail(int id)
+        {
+            Friend friend = _listOfFriends.getFriendById(id);
+            return View(friend);
+        }
+
+        public IActionResult DeleteFriend(int id)
+        {
+            _listOfFriends.deleteFriend(id);
+            return RedirectToAction("Index");
+            // Send it to a Are you sure page?
+        }
+
+        public IActionResult EditFriend(int id)
+        {
+            Friend friend = _listOfFriends.getFriendById(id);
+            return View(friend);
+        }
+
+        [HttpPost]
+        public IActionResult EditFriend(Friend friend)
+        {
+            _listOfFriends.editFriend(friend);
+            return View(friend);
+        }
+
+        //// UPDATE
+        ////public IActionResult EditFriend(int id)
+        ////{
+
+        //}
+
+        // DELETING
+    }
+}
